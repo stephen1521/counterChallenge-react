fc1587c6de9fb04489478ba8ef4ed75e57f530cb
stephen1521 <stephen.warnock@codeimmersives.com>
2023-05-29 14:24:05 -0400

    adding counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [stars, setStars] = useState([]);
+  const createStar = () => {
+    return (
+      <div className='stars'>
+        *
+      </div>
+    )
+  }
+  const handleAdd = () => {
+    setCount(count + 1);
+    setStars([...stars, createStar()])
+  }
+  
+  const handleMinus = () => {
+    if(count !== 0){
+      const end = stars.length - 1;
+      setCount(count - 1);
+      setStars(oldArr => {
+        return oldArr.filter((star, i) => {
+          if(i !== end){
+            return star;
+          }
+        });
+      })
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="outer-div">
+        <div id="inner-div">
+            <div id="banner">
+                <h3 id="logo">LOGO</h3>
+                <h3 id="menu">menu</h3>
+            </div>
+            <div id="display">
+                <button id="minus" onClick={() => handleMinus()}>remove 1</button>
+                <div id="number-display">{count}</div>
+                <button id="add" onClick={() => handleAdd()}>add 1</button>
+            </div>
+            <div id="stars-display">
+              {stars}
+            </div>
+        </div>
     </div>
   );
 }
